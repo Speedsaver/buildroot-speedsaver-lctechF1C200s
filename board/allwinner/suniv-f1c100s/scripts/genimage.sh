9c6cdba64d10f3152825d8c3ad43ed101b4faffb
Xogium <contact@xogium.me>
2022-03-12 10:33:02 +0100

    Initial rework of the original buildroot-mangopi repository to tweak for the Speedsaver build.

---
+++ board/allwinner/suniv-f1c100s/scripts/genimage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+SELFDIR=`dirname \`realpath ${0}\``
+
+[ $# -eq 2 ] || {
+    echo "SYNTAX: $0 <u-boot-with-spl image> <genimage.cfg>"
+    echo "Given: $@"
+    exit 1
+}
+
+${SELFDIR}/mknanduboot.sh ${1}/${2} ${1}/u-boot-sunxi-with-nand-spl.bin
+support/scripts/genimage.sh ${1} -c "${BR2_EXTERNAL_SPEEDSAVER_MANGOPI_PATH}/board/allwinner/suniv-f1c100s/genimage-nand.cfg"
