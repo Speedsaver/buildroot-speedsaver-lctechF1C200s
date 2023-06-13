607490e7817a563621e1bd36a62844d8dbbdc8e6
Aaron Elijah <aaronzakelijah@googlemail.com>
2023-06-13 20:12:20 +0100

    fix: edit kernel config to detect mmc partitions
    - Edit devicetree files to enable mmc module
    - Edit linux defconfig to install device drivers for mmc

--- board/allwinner/suniv-f1c100s/scripts/genimage.sh
+++ board/allwinner/suniv-f1c100s/scripts/genimage.sh
@@ -2,11 +2,23 @@
 set -e
 SELFDIR=`dirname \`realpath ${0}\``
 
+STARTDIR=`pwd`
+MKIMAGE="${HOST_DIR}/bin/mkimage"
+IMAGE_ITS="kernel.its"
+OUTPUT_NAME="kernel.itb"
+
 [ $# -eq 2 ] || {
     echo "SYNTAX: $0 <u-boot-with-spl image> <genimage.cfg>"
     echo "Given: $@"
     exit 1
 }
+
+cp "${BR2_EXTERNAL_SPEEDSAVER_MANGOPI_PATH}/board/allwinner/suniv-f1c100s/kernel.its" "${BINARIES_DIR}"
+cd "${BINARIES_DIR}"
+"${MKIMAGE}" -f ${IMAGE_ITS} ${OUTPUT_NAME}
+rm ${IMAGE_ITS}
+
+cd "${STARTDIR}/"
 
 ${SELFDIR}/mknanduboot.sh ${1}/${2} ${1}/u-boot-sunxi-with-nand-spl.bin
 support/scripts/genimage.sh ${1} -c "${BR2_EXTERNAL_SPEEDSAVER_MANGOPI_PATH}/board/allwinner/suniv-f1c100s/genimage-nand.cfg"
